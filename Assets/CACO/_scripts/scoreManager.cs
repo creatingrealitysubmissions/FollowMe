009e242d8107602a1f2b3e35f141075d2b3b450b
caco_ <eedlecrieg@gmail.com>
2018-03-14 00:31:28 -0700

    CACO-star-counter
    Star Counter Functionality

---
+++ Assets/CACO/_scripts/scoreManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreManager : MonoBehaviour {
+
+	public GameObject star1;
+	public GameObject star2;
+	public GameObject star3;
+	public GameObject star4;
+
+	public AudioSource starSuccess;
+
+	public int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		starSuccess = gameObject.GetComponent<AudioSource> ();
+		starSuccess.Play ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (score == 1) {
+			star1.SetActive (true);
+		}
+		if (score == 2) {
+			star2.SetActive (true);
+		}
+		if (score == 3) {
+			star3.SetActive (true);
+		}
+		if (score == 4) {
+			star4.SetActive (true);
+		}
+		
+	}
+
+	public void ScoreCounter(){
+		score++;
+		starSuccess.Play ();
+	}
+
+}
